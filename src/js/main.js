883681cf6d6c9216e77e18c5e1e60f22533bae85
yar-stun <studenikin.jarik@gmail.com>
2021-10-12 14:47:09 +0300

    fixed-nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
-console.log($('.socials__item'));
+const whiteSections = ["about", "contains", "request"];
+
+$(window).scroll(function () {
+  let $sections = $('section, footer');
+  $sections.each(function (i, el) {
+    let top = $(el).offset().top - 100;
+    let bottom = top + $(el).height();
+    let scroll = $(window).scrollTop();
+    let id = $(el).attr('id');
+    if (scroll > top && scroll < bottom) {
+      $('.anchor-nav__link.anchor-nav__link--active').removeClass('anchor-nav__link--active anchor-nav__link--active-white');
+      $('a.anchor-nav__link[href="#' + id + '"]').addClass('anchor-nav__link--active');
+      if (whiteSections.some(item => item === id)) {
+        $('a.anchor-nav__link[href="#' + id + '"]').addClass('anchor-nav__link--active-white');
+      }
+    }
+  })
+});
+
+$(".anchor-nav").on("click", "a", function (event) {
+  event.preventDefault();
+  let id = $(this).attr('href'),
+      top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 800);
+});
